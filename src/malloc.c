4b921ea546ebf40406d6d387363019b7d2d51415
Lucas Lefrancq <lucas.lefrancq@hotmail.fr>
2023-06-08 17:48:05 +0200

    tiny malloc and small malloc implemented, need to add the corresponding tests

--- src/malloc.c
+++ src/malloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   malloc.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llefranc <llefranc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lucaslefrancq <lucaslefrancq@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 12:17:26 by lucaslefran       #+#    #+#             */
-/*   Updated: 2023/06/01 19:02:25 by llefranc         ###   ########.fr       */
+/*   Updated: 2023/06/08 17:45:56 by lucaslefran      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,50 @@
 
 struct s_bins bins = { .tiny = NULL, .small = NULL, .large = NULL };
 
+static inline struct mmaphdr * create_bin(struct mmaphdr **head,
+					  size_t mmap_size)
+{
+	if (*head == NULL)
+		return lmmap_new(head, mmap_size);
+	return lmmap_push_back(*head, mmap_size);
+}
+
 static struct chkhdr * alloc_tiny(size_t size)
 {
-	(void)size;
-	return NULL;
+	struct mmaphdr *new_m;
+	struct chkhdr *chk;
+
+	size = chk_size_16align(size);
+	if ((chk = lmmap_bestfit(bins.tiny, size)) == NULL) {
+		if ((new_m = create_bin(&bins.tiny, TINY_MMAP_SIZE)) == NULL)
+			return NULL;
+		chk = new_m->first_free.next_free;
+	}
+	return chk_alloc(chk, size);
 }
 
 static struct chkhdr * alloc_small(size_t size)
 {
-	(void)size;
-	return NULL;
+	struct mmaphdr *new_m;
+	struct chkhdr *chk;
+
+	size = chk_size_16align(size);
+	if ((chk = lmmap_bestfit(bins.small, size)) == NULL) {
+		if ((new_m = create_bin(&bins.small, SMALL_MMAP_SIZE)) == NULL)
+			return NULL;
+		chk = new_m->first_free.next_free;
+	}
+	return chk_alloc(chk, size);
 }
 
 /**
- * Allocate a large area of memory, consisting of 1 mmap area with only one 
+ * Allocate a large area of memory, consisting of 1 mmap area with only one
  * chunk inside filling all the space.
 */
 static struct chkhdr * alloc_large(size_t size)
 {
-	/* 
-	 * One boundary tag is already inside struct mmaphdr, but we need to 
+	/*
+	 * One boundary tag is already inside struct mmaphdr, but we need to
 	 * count the last byte of the mmap area not used because of 16 align
 	 * chunks: the last footer will be align on 16, and so there is a last
 	 * unused byte. So we do BNDARY_TAG_SIZE * 2 to count it.
@@ -48,16 +72,10 @@ static struct chkhdr * alloc_large(size_t size)
 	struct chkhdr *new_c;
 	struct mmaphdr *new_m;
 
-	if (bins.large == NULL) {
-		new_m = lmmap_new(&bins.large, mmap_size);
-	} else {
-		new_m = lmmap_push_back(bins.large, mmap_size);
-	}
-	if (!new_m)
+	if ((new_m = create_bin(&bins.large, mmap_size)) == NULL)
 		return NULL;
-	
-	/* 
-	 * Large allocated chunk will fit the whole mmap size because this 
+	/*
+	 * Large allocated chunk will fit the whole mmap size because this
 	 * space is lost anyway.
 	 */
 	size = new_m->size - (sizeof(struct mmaphdr) + BNDARY_TAG_SIZE * 2);
